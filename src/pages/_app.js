b84f3a584d4c2d6392795b3fee5be8974fadb505
Troels <troels@blck.dk>
2021-10-09 20:42:40 +0200

    copy

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,6 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import {breakpoint} from 'styled-components-breakpoint';
 import theme from 'base/style';
 import Web3, { utils } from "web3";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import { InjectedConnector } from "@web3-react/injected-connector";
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { Web3ReactProvider, useWeb3React } from "@web3-react/core";
 
 
