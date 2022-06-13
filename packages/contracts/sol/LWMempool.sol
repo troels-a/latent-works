fb5757424588b27d811ac78e66f0e6bc88587472
Troels Abrahamsen <troels@blck.dk>
2022-06-13 17:45:18 +0200

    wip

--- packages/contracts/sol/LWMempool.sol
+++ packages/contracts/sol/LWMempool.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import './LTNT.sol';
 import './lib/Rando.sol';
 
-contract LWMempool {
+contract LWMempool is ERC721 {
 
     struct Pool {
         bytes items;
@@ -12,12 +12,31 @@ contract LWMempool {
         string seed;
         string seed1;
         string seed2;
+        uint epoch;
+        uint max;
     }
 
+    uint public constant MAX_EPOCHS = 20;
+
+    uint private _pool_ids;
+    mapping(uint => uint) private _pool_timestamps;
+    mapping(uint => address) private _pool_minters;
     string[] private _bases;
 
-    constructor(){
+    constructor() ERC721("Mempools: issue 1", "MEMPOOLS1"){
 
+    }
+
+
+    function mint() public {
+
+        _pool_ids++;
+
+        _mint(msg.sender, _pool_ids);
+
+        _pool_timestamps[_pool_ids] = block.timestamp;
+        _pool_minters[_pool_ids] = msg.sender;
+        
     }
 
 
@@ -25,20 +44,36 @@ contract LWMempool {
         _bases.push(base_);
     }
 
+    function getSeed(uint pool_id_, string memory append_) public view returns(string memory){
+        return string(abi.encodePacked(Strings.toString(_pool_timestamps[pool_id_]), append_));
+    }
 
-    function generateImage(string memory seed_) public view returns(string memory){
+    function getEpochLength(uint pool_id_) public view returns(uint){
+        return Rando.number(getSeed(pool_id_, ''), 1, 6)*7776000;
+    }
+
+    function getCurrentEpoch(uint pool_id_) public view returns(uint){
+        uint epoch = ((block.timestamp - _pool_timestamps[pool_id_]) / getEpochLength(pool_id_))+1;
+        if(epoch > MAX_EPOCHS)
+            return MAX_EPOCHS;
+        return epoch;
+    }
+
+    function getEpochImage(uint pool_id_, uint epoch_, bool encode_) public view returns(string memory){
 
         Pool memory pool_;
+        pool_.epoch = getCurrentEpoch(pool_id_); // Advances in different increments for each
 
-        pool_.base = _bases[Rando.number(seed_, 0, _bases.length)];
+        require(epoch_ <= pool_.epoch, 'EPOCH_NOT_REACHED');
 
-        uint age_ = 30; // Advances in different increments for each
-        uint max = Rando.number(seed_, age_+1, age_+2); // Increase over time
+        pool_.seed = getSeed(pool_id_, 'base_seed');
+        pool_.base = _bases[Rando.number(pool_.seed, 0, _bases.length)];
+        pool_.max = Rando.number(pool_.seed, pool_.epoch+5, pool_.epoch*2); // Increase over time
 
         uint i;
-        while(i < max){
-            pool_.seed1 = string(abi.encodePacked(seed_, '1x', Strings.toString(i)));
-            pool_.seed2 = string(abi.encodePacked(seed_, '1y', Strings.toString(i)));
+        while(i < pool_.max){
+            pool_.seed1 = getSeed(pool_id_, string(abi.encodePacked('1x', Strings.toString(i))));
+            pool_.seed2 = getSeed(pool_id_, string(abi.encodePacked('1y', Strings.toString(i))));
             pool_.items = abi.encodePacked(
                 pool_.items,
                 '<use href="#shape',Strings.toString(Rando.number(pool_.seed1, 1, 3)),'" x="',Strings.toString(Rando.number(pool_.seed1, 1, 990)),'" y="',Strings.toString(Rando.number(pool_.seed2, 1, 900)),'" fill="url(#base',Strings.toString(Rando.number(pool_.seed2, 1, 5)),')"/>'
@@ -48,9 +83,9 @@ contract LWMempool {
         }
 
         i = 0;
-        while(i < max){
-            pool_.seed1 = string(abi.encodePacked(seed_, '2x', Strings.toString(i)));
-            pool_.seed2 = string(abi.encodePacked(seed_, '2y', Strings.toString(i)));
+        while(i < pool_.max){
+            pool_.seed1 = getSeed(pool_id_, string(abi.encodePacked('2x', Strings.toString(i))));
+            pool_.seed2 = getSeed(pool_id_, string(abi.encodePacked('2y', Strings.toString(i))));
             pool_.items = abi.encodePacked(
                 pool_.items,
                 '<use href="#shape',Strings.toString(Rando.number(pool_.seed1, 1, 3)),'" x="',Strings.toString(Rando.number(pool_.seed1, 1, 990)),'" y="',Strings.toString(Rando.number(pool_.seed2, 1, 900)),'" fill="url(#base',Strings.toString(Rando.number(pool_.seed2, 1, 5)),')"/>'
@@ -60,9 +95,9 @@ contract LWMempool {
         }
 
         i = 0;
-        while(i < max){
-            pool_.seed1 = string(abi.encodePacked(seed_, '3x', Strings.toString(i)));
-            pool_.seed2 = string(abi.encodePacked(seed_, '3y', Strings.toString(i)));
+        while(i < pool_.max){
+            pool_.seed1 = getSeed(pool_id_, string(abi.encodePacked('3x', Strings.toString(i))));
+            pool_.seed2 = getSeed(pool_id_, string(abi.encodePacked('3y', Strings.toString(i))));
             pool_.items = abi.encodePacked(
                 pool_.items,
                 '<use href="#shape',Strings.toString(Rando.number(pool_.seed1, 1, 3)),'" x="',Strings.toString(Rando.number(pool_.seed1, 1, 990)),'" y="',Strings.toString(Rando.number(pool_.seed2, 1, 900)),'" fill="url(#base',Strings.toString(Rando.number(pool_.seed2, 1, 5)),')"/>'
@@ -76,9 +111,9 @@ contract LWMempool {
                 '<defs>',
                     '<style></style><circle cx="500" cy="500" r="500" id="bg"/>',
                     '<clipPath id="clip"><use href="#bg"/></clipPath>',
-                    '<rect id="shape1" width="',Strings.toString(Rando.number(string(abi.encodePacked(seed_, 'shape1line')), 1, 50)),'" height="',Strings.toString(Rando.number(string(abi.encodePacked(seed_, 'shape1')), 20, 300)),'"/>',
-                    '<rect id="shape2" width="30" height="',Strings.toString(Rando.number(string(abi.encodePacked(seed_, 'shape2')), 20, 200)),'"/>',
-                    '<rect id="shape3" width="20" height="',Strings.toString(Rando.number(string(abi.encodePacked(seed_, 'shape3')), 20, 100)),'"/>',
+                    '<rect id="shape1" width="',Strings.toString(Rando.number(getSeed(pool_id_, 'shape1width'), 1, 50)),'" height="',Strings.toString(Rando.number(getSeed(pool_id_, 'shape1height'), 20, 300)),'"/>',
+                    '<rect id="shape2" width="30" height="',Strings.toString(Rando.number(getSeed(pool_id_, 'shape2width'), 20, 200)),'"/>',
+                    '<rect id="shape3" width="20" height="',Strings.toString(Rando.number(getSeed(pool_id_, 'shape3width'), 20, 100)),'"/>',
                     '<image id="base" width="1000" height="1000" href="',pool_.base,'"/>',
                     '<pattern id="base1" x="0" y="0" width="1" height="1" viewBox="0 0 200 200"><use href="#base"/></pattern>',
                     '<pattern id="base2" x="0" y="0" width="1" height="1" viewBox="200 200 200 200" preserveAspectRatio="xMidYMid slice"><use href="#base"/></pattern>',
@@ -89,18 +124,21 @@ contract LWMempool {
                 '</defs>',
                 '<g clip-path="url(#clip)">',
                 '<use href="#bg" fill="white"/>',
-                '<use href="#bg" fill-opacity="0.8" fill="url(#base',Strings.toString(Rando.number(string(abi.encodePacked(seed_, 'bg1')), 2, 5)),')"/>',
-                '<use href="#bg" fill-opacity="0.8" fill="url(#base',Strings.toString(Rando.number(string(abi.encodePacked(seed_, 'bg2')), 2, 5)),')"/>',
+                '<use href="#bg" fill-opacity="0.8" fill="url(#base',Strings.toString(Rando.number(getSeed(pool_id_, 'bg1'), 2, 5)),')"/>',
+                '<use href="#bg" fill-opacity="0.8" fill="url(#base',Strings.toString(Rando.number(getSeed(pool_id_, 'bg2'), 2, 5)),')"/>',
                 '<g filter="url(#blur)" transform="translate(0, -100)" id="pool">',
                 pool_.items,
                 '</g>',
                 '<use href="#pool" transform="scale(.5, 0.5)"/>',
-                '<use href="#pool" transform="scale(.5, 0.5) translate(0, 1000)"/>'
+                '<use href="#pool" transform="scale(.5, 0.5) translate(',Strings.toString(Rando.number(pool_.seed, 0, 100)),', 1000)"/>'
                 '<use href="#pool" transform="scale(0.8, 0.8) translate(1000, 0)"/>'
-                '<use href="#pool"  transform="scale(1, 1.5) translate(',Strings.toString(Rando.number(pool_.seed1, 0, 500)),', ',Strings.toString(Rando.number(string(abi.encodePacked(pool_.seed1)), 0, 500)),')"/>',
-                '</g>'
+                '<use href="#pool"  transform="scale(1, 1.5) translate(',Strings.toString(Rando.number(pool_.seed, 0, 500)),', ',Strings.toString(Rando.number(pool_.seed, 0, 500)),')"/>',
+                '</g>',
             '</svg>'
         );
+
+        if(encode_)
+            return Base64.encode(svg_);
 
         return string(svg_);
 
--- packages/contracts/test/mempool.js
+++ packages/contracts/test/mempool.js
@@ -4,11 +4,13 @@ const fs = require('fs');
 const path = require('path').dirname(__dirname);
 const Preview = require('../preview.js');
 
+const MINT_MAX = 9;
+
 
 describe('mempool', async function(){
 
     let _mempool, preview_dir;
-
+    
     it('init', async function(){
 
         const LWMempool = await hre.ethers.getContractFactory("LWMempool");
@@ -16,7 +18,7 @@ describe('mempool', async function(){
         await _mempool.deployed();
 
         preview_dir = `${path}/temp/preview`;
-        base_dir = `${path}/temp/base/pool`;
+        base_dir = `${path}/temp/base/color`;
         await fs.promises.mkdir(preview_dir, {recursive: true}).catch(console.error);
         const files = await fs.readdirSync(base_dir);
         
@@ -29,15 +31,33 @@ describe('mempool', async function(){
 
     })
 
+    it('mints', async function(){
+        let i = 1;
+        while(i <= MINT_MAX){
+            await _mempool.mint();
+            i++;
+        }
+    })
+
     it('generates', async function(){
+        
         this.timeout(500000);
 
-        const max = 9;
-        let i = 1;
+        const pool_id = 1;
+        const increase = await _mempool.getEpochLength(pool_id);
 
-        while(i <= max){
-            let svg = await _mempool.generateImage((256*i)+(100000/i)+'');
-            await fs.writeFileSync(`${preview_dir}/mempool_${i}.svg`, svg, {flag: 'w'});
+        let epoch = await _mempool.getCurrentEpoch(pool_id);
+        let i = 1;
+        let max_epochs = await _mempool.MAX_EPOCHS();
+        max_epochs = max_epochs.toNumber();
+
+        while(epoch <= max_epochs){
+            const epoch = await _mempool.getCurrentEpoch(pool_id);
+            console.log(i, epoch.toNumber());
+            const svg = await _mempool.getEpochImage(pool_id, epoch, false);
+            await fs.writeFileSync(`${preview_dir}/mempool_${pool_id}_${epoch}.svg`, svg, {flag: 'w'});
+            await network.provider.send("evm_increaseTime", [increase.toNumber()])
+            await network.provider.send("evm_mine") // this one will have 02:00 PM as its timestamp
             i++;
         }
 
