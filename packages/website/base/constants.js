d236a8a2945250496fb102803198947b9002f260
Troels Abrahamsen <troels@blck.dk>
2022-10-26 14:13:16 +0200

    Site description

--- packages/website/base/constants.js
+++ packages/website/base/constants.js
@@ -1,3 +1,3 @@
 export const TITLE = 'Latent works';
-export const DESCRIPTION = '77 generative artworks';
+export const DESCRIPTION = 'An ongoing project exploring permanence, shared ownership and expansive formats in the context of decentralised internet systems';
 export const URL = 'https://latent.works'
