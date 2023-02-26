79a13caaf17e7f57a7580fb50db717c3b4627b98
Troels Abrahamsen <troels@blck.dk>
2023-02-26 01:57:12 +0100

    epoch info

--- packages/website/pages/mempools/index.js
+++ packages/website/pages/mempools/index.js
@@ -337,7 +337,7 @@ const PoolStats = styled(({id, ...p}) => {
     left: 0;
     right: 0;
     top: 0;
-    background-color: rgba(0,0,0,0.5);
+    background-color: transparent;
     color: ${p => p.theme.colors.txt};
     > div {
         flex: 1;
